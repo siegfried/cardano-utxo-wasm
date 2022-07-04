d7b2ad17018124f90e4595310c532af0e18f378d
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-07-04 15:15:53 +0800

    Use typed setters for Javascript objects

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ UTxO helpers for Cardano in WASM
 This package wraps UTxO helpers written in Rust into WASM
 so that they can be used by Nodejs and the browsers.
 */
-use js_sys::{try_iter, Array, BigInt, Object, Reflect};
+use js_sys::{try_iter, Array, Object};
 use std::collections::BTreeMap;
 use utxo::{try_sum, ExtOutput};
 use wasm_bindgen::{prelude::*, JsCast};
@@ -43,8 +43,14 @@ extern "C" {
     #[wasm_bindgen(method, getter)]
     fn hash(this: &JsTransactionID) -> String;
 
+    #[wasm_bindgen(method, setter)]
+    fn set_hash(this: &JsTransactionID, hash: &str);
+
     #[wasm_bindgen(method, getter)]
     fn index(this: &JsTransactionID) -> u32;
+
+    #[wasm_bindgen(method, setter)]
+    fn set_index(this: &JsTransactionID, index: u32);
 
     #[wasm_bindgen(typescript_type = "Asset")]
     type JsAsset;
@@ -52,27 +58,44 @@ extern "C" {
     #[wasm_bindgen(method, getter = policyId)]
     fn policy_id(this: &JsAsset) -> String;
 
+    #[wasm_bindgen(method, setter = policyId)]
+    fn set_policy_id(this: &JsAsset, policy_id: &str);
+
     #[wasm_bindgen(method, getter = assetName)]
     fn asset_name(this: &JsAsset) -> String;
+
+    #[wasm_bindgen(method, setter = assetName)]
+    fn set_asset_name(this: &JsAsset, asset_name: &str);
 
     #[wasm_bindgen(method, getter)]
     fn quantity(this: &JsAsset) -> u64;
 
+    #[wasm_bindgen(method, setter)]
+    fn set_quantity(this: &JsAsset, quantity: u64);
+
     #[wasm_bindgen(typescript_type = "Array<Asset>")]
     type JsAssetArray;
 
-    #[derive(Clone)]
     #[wasm_bindgen(typescript_type = "Output")]
     pub type JsOutput;
 
     #[wasm_bindgen(method, getter)]
     fn id(this: &JsOutput) -> Option<JsTransactionID>;
 
+    #[wasm_bindgen(method, setter)]
+    fn set_id(this: &JsOutput, id: &JsTransactionID);
+
     #[wasm_bindgen(method, getter)]
     fn lovelace(this: &JsOutput) -> u64;
 
+    #[wasm_bindgen(method, setter)]
+    fn set_lovelace(this: &JsOutput, lovelace: u64);
+
     #[wasm_bindgen(method, getter)]
     fn assets(this: &JsOutput) -> JsAssetArray;
+
+    #[wasm_bindgen(method, setter)]
+    fn set_assets(this: &JsOutput, assets: &JsAssetArray);
 
     #[wasm_bindgen(typescript_type = "Array<Output>")]
     pub type JsOutputArray;
@@ -83,40 +106,56 @@ extern "C" {
     #[wasm_bindgen(method, getter)]
     fn selected(this: &SelectResult) -> JsOutputArray;
 
+    #[wasm_bindgen(method, setter)]
+    fn set_selected(this: &SelectResult, selected: &JsOutputArray);
+
     #[wasm_bindgen(method, getter)]
     fn unselected(this: &SelectResult) -> JsOutputArray;
 
+    #[wasm_bindgen(method, setter)]
+    fn set_unselected(this: &SelectResult, unselected: &JsOutputArray);
+
     #[wasm_bindgen(method, getter)]
     fn excess(this: &SelectResult) -> JsOutput;
+
+    #[wasm_bindgen(method, setter)]
+    fn set_excess(this: &SelectResult, excess: &JsOutput);
 }
 
-type TransactionID = (String, u32);
-type AssetID = (String, String);
-pub type Output = ExtOutput<TransactionID, AssetID>;
+pub type Output<'o> = ExtOutput<&'o JsOutput, (String, String)>;
 
-impl From<JsTransactionID> for TransactionID {
-    fn from(value: JsTransactionID) -> Self {
-        (value.hash(), value.index())
+struct Asset<'a> {
+    policy_id: &'a str,
+    asset_name: &'a str,
+    quantity: u64,
+}
+
+impl<'a> Asset<'a> {
+    fn new(policy_id: &'a str, asset_name: &'a str, quantity: u64) -> Self {
+        Self {
+            policy_id,
+            asset_name,
+            quantity,
+        }
     }
 }
 
-impl From<TransactionID> for JsTransactionID {
-    fn from(value: TransactionID) -> Self {
-        let object = Object::new();
-
-        Reflect::set(&object, &"hash".into(), &value.0.into()).unwrap();
-        Reflect::set(&object, &"index".into(), &value.1.into()).unwrap();
-
-        object.unchecked_into()
+impl From<Asset<'_>> for JsAsset {
+    fn from(value: Asset) -> Self {
+        let id: Self = Object::new().unchecked_into();
+        id.set_policy_id(value.policy_id);
+        id.set_asset_name(value.asset_name);
+        id.set_quantity(value.quantity);
+        id
     }
 }
 
-impl From<JsOutput> for Output {
-    fn from(value: JsOutput) -> Self {
+impl<'o> From<&'o JsOutput> for Output<'o> {
+    fn from(value: &'o JsOutput) -> Self {
         let mut output = Self {
-            id: value.id().and_then(|i| Some(i.into())),
             value: value.lovelace(),
             assets: BTreeMap::new(),
+            data: Some(value),
         };
 
         if let Some(assets) = try_iter(&value.assets()).unwrap() {
@@ -130,35 +169,21 @@ impl From<JsOutput> for Output {
     }
 }
 
-impl From<Output> for JsOutput {
+impl From<Output<'_>> for JsOutput {
     fn from(value: Output) -> Self {
-        let object = Object::new();
+        let js_output: Self = Object::new().unchecked_into();
 
-        if let Some(id) = value.id {
-            let id: JsTransactionID = id.into();
-            Reflect::set(&object, &"id".into(), &id).unwrap();
-        }
-
-        Reflect::set(
-            &object,
-            &JsValue::from_str(&"lovelace"),
-            &value.value.into(),
-        )
-        .unwrap();
+        js_output.set_lovelace(value.value);
 
         let assets = Array::new();
         for ((policy_id, asset_name), quantity) in value.assets.into_iter() {
-            let object = Object::new();
-
-            Reflect::set(&object, &"policyId".into(), &policy_id.into()).unwrap();
-            Reflect::set(&object, &"assetName".into(), &asset_name.into()).unwrap();
-            Reflect::set(&object, &"quantity".into(), &BigInt::from(quantity)).unwrap();
-
-            assets.push(&object);
+            let asset: JsAsset = Asset::new(&policy_id, &asset_name, quantity).into();
+            assets.push(&asset);
         }
-        Reflect::set(&object, &"assets".into(), &assets).unwrap();
+        let assets: JsAssetArray = assets.unchecked_into();
+        js_output.set_assets(&assets);
 
-        object.unchecked_into()
+        js_output
     }
 }
 
@@ -179,33 +204,34 @@ pub fn select(
     outputs: &JsOutputArray,
     threshold: &JsOutput,
 ) -> Result<Option<SelectResult>, JsError> {
-    let mut inputs: Vec<Output> = try_iter(inputs)
+    let js_inputs: Vec<JsOutput> = try_iter(inputs)
         .unwrap()
         .unwrap()
         .into_iter()
-        .map(|i| i.unwrap().unchecked_into::<JsOutput>().into())
+        .map(|i| i.unwrap().unchecked_into())
         .collect();
-    let outputs: Vec<Output> = try_iter(outputs)
+    let mut inputs: Vec<Output> = js_inputs.iter().map(|o| o.into()).collect();
+    let js_outputs: Vec<JsOutput> = try_iter(outputs)
         .unwrap()
         .unwrap()
         .into_iter()
-        .map(|i| i.unwrap().unchecked_into::<JsOutput>().into())
+        .map(|i| i.unwrap().unchecked_into())
         .collect();
-    let threshold: Output = threshold.clone().into();
+    let outputs: Vec<Output> = js_outputs.iter().map(|o| o.into()).collect();
+    let threshold: Output = threshold.into();
     let total_output: Output =
         try_sum(&outputs).ok_or_else(|| JsError::new(&"Outputs overflowed"))?;
 
     Ok(
         utxo::select(&mut inputs[..], &total_output, &threshold).and_then(
             |(selected, unselected, excess)| {
-                let result = Object::new();
+                let result: SelectResult = Object::new().unchecked_into();
 
                 let selected: JsOutputArray = {
                     let result = Array::new();
 
                     for output in selected {
-                        let js_output: JsOutput = output.clone().into();
-                        result.push(&js_output);
+                        result.push(output.data.unwrap());
                     }
 
                     result.unchecked_into()
@@ -215,8 +241,7 @@ pub fn select(
                     let result = Array::new();
 
                     for output in unselected {
-                        let js_output: JsOutput = output.clone().into();
-                        result.push(&js_output);
+                        result.push(output.data.unwrap());
                     }
 
                     result.unchecked_into()
@@ -224,11 +249,11 @@ pub fn select(
 
                 let excess: JsOutput = excess.into();
 
-                Reflect::set(&result, &"selected".into(), &selected.into()).unwrap();
-                Reflect::set(&result, &"unselected".into(), &unselected.into()).unwrap();
-                Reflect::set(&result, &"excess".into(), &excess.into()).unwrap();
+                result.set_selected(&selected);
+                result.set_unselected(&unselected);
+                result.set_excess(&excess);
 
-                Some(result.unchecked_into())
+                Some(result)
             },
         ),
     )
@@ -236,8 +261,11 @@ pub fn select(
 
 #[cfg(test)]
 mod tests {
-    use crate::{select, JsOutput, JsOutputArray, Output, SelectResult};
-    use js_sys::{try_iter, Array};
+    use crate::{
+        select, Asset, JsAsset, JsAssetArray, JsOutput, JsOutputArray, JsTransactionID, Output,
+        SelectResult,
+    };
+    use js_sys::{try_iter, Array, Object};
     use std::collections::BTreeMap;
     use utxo::Select;
     use wasm_bindgen::JsCast;
@@ -245,19 +273,85 @@ mod tests {
 
     wasm_bindgen_test_configure!(run_in_browser);
 
+    struct TransactionID<'a> {
+        hash: &'a str,
+        index: u32,
+    }
+
+    impl<'a> TransactionID<'a> {
+        fn new(hash: &'a str, index: u32) -> Self {
+            Self { hash, index }
+        }
+    }
+
+    impl From<TransactionID<'_>> for JsTransactionID {
+        fn from(value: TransactionID) -> Self {
+            let id: Self = Object::new().unchecked_into();
+            id.set_hash(value.hash);
+            id.set_index(value.index);
+            id
+        }
+    }
+
     #[wasm_bindgen_test]
-    fn test_output_converting() {
+    fn test_from_js_output_to_output() {
+        let js_output: JsOutput = Object::new().unchecked_into();
+        js_output.set_id(&TransactionID::new("hash0", 0).into());
+        js_output.set_lovelace(1000);
+        let js_assets: JsAssetArray = {
+            let assets = Array::new();
+
+            let asset: JsAsset = Asset::new("policy1", "aname1", 10000).into();
+            assets.push(&asset);
+
+            let asset: JsAsset = Asset::new("policy2", "aname2", 100000).into();
+            assets.push(&asset);
+
+            assets.unchecked_into()
+        };
+        js_output.set_assets(&js_assets);
+
+        assert_eq!(js_output.lovelace(), 1000);
+        assert_eq!(js_output.id().unwrap().hash(), "hash0".to_string());
+        assert_eq!(js_output.id().unwrap().index(), 0);
+        assert_eq!(js_output.assets().unchecked_into::<Array>().length(), 2);
+
+        let output: Output = (&js_output).into();
+
+        assert_eq!(output.value, 1000);
+        assert_eq!(output.assets.len(), 2);
+        assert_eq!(
+            output
+                .assets
+                .get(&("policy1".into(), "aname1".into()))
+                .unwrap(),
+            &10000
+        );
+        assert_eq!(
+            output
+                .assets
+                .get(&("policy2".into(), "aname2".into()))
+                .unwrap(),
+            &100000
+        );
+        assert!(output.data.unwrap().loose_eq(&js_output));
+    }
+
+    #[wasm_bindgen_test]
+    fn test_from_output_to_js_output() {
         let mut output = Output {
-            id: Some(("hash1".into(), 1)),
             value: 1000,
             assets: BTreeMap::new(),
+            data: None,
         };
         output.insert_asset(("policy1".into(), "aname1".into()), 10000);
         output.insert_asset(("policy2".into(), "aname2".into()), 100000);
-        let js_output: JsOutput = output.clone().into();
-        let result: Output = js_output.into();
 
-        assert_eq!(output, result);
+        let js_output: JsOutput = output.into();
+
+        assert_eq!(js_output.lovelace(), 1000);
+        assert_eq!(js_output.assets().unchecked_into::<Array>().length(), 2);
+        assert!(js_output.id().is_none());
     }
 
     #[wasm_bindgen_test]
@@ -266,9 +360,9 @@ mod tests {
             let result = Array::new();
 
             let mut output = Output {
-                id: None,
                 value: 1000,
                 assets: BTreeMap::new(),
+                data: None,
             };
             output.insert_asset(("policy1".into(), "aname1".into()), 10000);
             output.insert_asset(("policy2".into(), "aname2".into()), 100000);
@@ -277,9 +371,9 @@ mod tests {
             result.push(&output);
 
             let output = Output {
-                id: None,
                 value: 5000,
                 assets: BTreeMap::new(),
+                data: None,
             };
 
             let output: JsOutput = output.into();
@@ -292,34 +386,37 @@ mod tests {
             let result = Array::new();
 
             let mut output = Output {
-                id: Some(("hash1".into(), 1)),
                 value: 10000,
                 assets: BTreeMap::new(),
+                data: None,
             };
             output.insert_asset(("policy3".into(), "aname1".into()), 10000);
             output.insert_asset(("policy4".into(), "aname2".into()), 100000);
 
             let output: JsOutput = output.into();
+            output.set_id(&TransactionID::new("hash1", 1).into());
             result.push(&output);
 
             let mut output = Output {
-                id: Some(("hash2".into(), 2)),
                 value: 200,
                 assets: BTreeMap::new(),
+                data: None,
             };
             output.insert_asset(("policy1".into(), "aname1".into()), 20000);
             output.insert_asset(("policy2".into(), "aname2".into()), 200000);
 
             let output: JsOutput = output.into();
+            output.set_id(&TransactionID::new("hash2", 2).into());
             result.push(&output);
 
             let output = Output {
-                id: Some(("hash3".into(), 3)),
                 value: 7000,
                 assets: BTreeMap::new(),
+                data: None,
             };
 
             let output: JsOutput = output.into();
+            output.set_id(&TransactionID::new("hash3", 3).into());
             result.push(&output);
 
             result.unchecked_into()
